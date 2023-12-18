f5041c45be78d9268b73003e9174751178b396c2
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-18 19:51:17 +0530

    added text

---
+++ Delete_old_logs.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+SOURCE_DIR =/tmp/Script-logs
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
+N="e\[0m"
+
+if [ ! -d $SOURCE_DIR ] ;
+then
+echo "$SOURCE_DIR  directory $R not exist"
+else
+echo "$G exist"
+
