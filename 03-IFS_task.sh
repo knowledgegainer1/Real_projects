63f7321cf43f2eaad4fb2a18c6bb89aaf45ed306
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-20 00:15:04 +0530

    aa

--- 03-IFS_task.sh
+++ 03-IFS_task.sh
@@ -7,12 +7,12 @@ G="\e[32m" #added colours
 Y="\e[33m"
 N="\e[0m"
 
-if [ ! -f $file_loc ]; then
+if [ ! -f $file_loc ]; then    # -f to check whether givrn file is a file or not
     echo -e "$file_loc doesn't Exist"
 fi
 
-while IFS=":" read -r f1 f2
+while IFS="/" read -r f1 f2        #here fields are seperated by : and send into fragments f1 f2 each fragment each : value 
 do
 echo -e " $f1  this is user name "
 echo -e " $f2  this is next full text "
-done < $file_loc
+done < $file_loc   #input from file
