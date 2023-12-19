f310ceb68a6cdcd734a6db6f2cce6ef5528022ef
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-20 00:05:53 +0530

    new file created

---
+++ 03-IFS_task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_loc="etc/passwd"
+
+R="\e[31m"
+G="\e[32m" #added colours
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file_loc]; then
+    echo -e "$file_loc doesn't Exist"
+fi
+
+while IFS=":"  read -r fi
+do
+echo -e "$fi"
+done < $file_loc
--- Delete_old_logs.sh
+++ Delete_old_logs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-SOURCE_DIR="/tmp/Script-logs"
+SOURCE_DIR="/tmp/Script-logs"   #variable created no space while creating varianles Careul
 R="\e[31m"
-G="\e[32m"
+G="\e[32m"  #added colours
 Y="\e[33m"
 N="\e[0m"
 
-if [ ! -d $SOURCE_DIR ]; 
+if [ ! -d $SOURCE_DIR ];   #if condition to check if folider exist and negative it means folder does not exist
 then
     echo -e " $SOURCE_DIR  Directory $R Not Exist "
 fi
 #variables color is blue heren
-files_to_delete=$(find $SOURCE_DIR -type f -mtime +14 -name  "*.log")
+files_to_delete=$(find $SOURCE_DIR -type f -mtime +14 -name  "*.log") #to find files 14days ago on .log files 
 
-while IFS= read -r line
+while IFS= read -r line  #while loop for deleting all files existinf and IFS is used only when o/p from a cmd
 do
 echo -e  "deleting... $R $line $N"
-rm -rf $line
+rm -rf $line # -r -f means resursive forcly remove without any prompt
 
-done <<< $files_to_delete
+done <<< $files_to_delete  #input from above variable to loop
