15e59627f06b0b797e01576889c90ec91789cf7b
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-19 23:23:36 +0530

    added in string format for directry ""

--- Delete_old_logs.sh
+++ Delete_old_logs.sh
@@ -11,9 +11,11 @@ then
     echo -e " $SOURCE_DIR  Directory $R Not Exist "
 fi
 #variables color is blue heren
-files_to_delete=$(find /tmp/Script-logs/ -type f -mtime +14 -name  "*.log")
+files_to_delete=$(find $SOURCE_DIR -type f -mtime +14 -name  "*.log")
 
 while IFS= read -r line
 do
-echo -e  "deleting $R $line $N"
+echo -e  "deleting... $R $line $N"
+rm -rf $line
+
 done <<< $files_to_delete
