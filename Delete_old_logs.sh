f43bc48d1a49f5c0066aa3cbdbe7d2b3ef759d06
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-19 22:44:02 +0530

    added in string format for directry ""

--- Delete_old_logs.sh
+++ Delete_old_logs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-SOURCE_DIR ="/tmp/Script-logs"
+SOURCE_DIR="/tmp/Script-logs"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ ! -d "$SOURCE_DIR" ]; then
-    echo -e " '$SOURCE_DIR'  Directory $R Not Exist "
+if [ ! -d $SOURCE_DIR ]; 
+then
+    echo -e " $SOURCE_DIR  Directory $R Not Exist "
 fi
