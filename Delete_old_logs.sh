5a19cbfe3c6188af0355c0322226565e24caa71c
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-19 23:17:20 +0530

    added in string format for directry ""

--- Delete_old_logs.sh
+++ Delete_old_logs.sh
@@ -15,5 +15,5 @@ files_to_delete=$(find /tmp/Script-logs/ -type f -mtime +14 -name  "*.log")
 
 while IFS= read -r line
 do
-echo "deleting $R $line"
+echo -e  "deleting $R $line"
 done <<< $files_to_delete
