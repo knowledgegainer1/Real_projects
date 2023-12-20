aab58b5ad469392a833f9d48c996e16552e45aab
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-21 00:55:42 +0530

    adding new file

--- mail.sh
+++ mail.sh
@@ -6,6 +6,6 @@ message=$3
 clonemaker=$4
  Alert=$5
 
- final=$(sed -e 's/Team/$DevOps/g' -e 's/alertmessage/$High/g' -e 's/BODY/$message/g' temp.html )
+ final=$(sed -e "s/Team/$DevOps/g" -e "s/alertmessage/$High/g" -e "s/BODY/$message/g" temp.html )
 
  echo "$final" | mail -s "$(echo -e "$Alert\nContent-Type: text/html")" "$clonemaker"
