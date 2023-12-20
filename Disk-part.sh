9285d85b91f7bdee0d950940da744c71365ce9a3
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-21 01:02:57 +0530

    adding new file

--- Disk-part.sh
+++ Disk-part.sh
@@ -8,7 +8,7 @@ while IFS= read line; do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $disk_treshold ]; then
-        message+="high disk usage on $partition: $usage \n"
+        message+="high disk usage on $partition: $usage <br>"
     fi
 done <<<$disk_usage
 
--- mail.sh
+++ mail.sh
@@ -7,6 +7,6 @@ escape=$(printf '%s\n' "$message" | sed -e 's/[]\/$*.^[]/\\&/g');
 clonemaker=$4
  Alert=$5
 
- final=$(sed -e "s/Team/$DevOps/g" -e "s/alertmessage/$High/g" -e "s/BODY/$message/g" temp.html )
+ final=$(sed -e "s/Team/$DevOps/g" -e "s/alertmessage/$High/g" -e "s/BODY/$escape/g" temp.html )
 
  echo "$final" | mail -s "$(echo -e "$Alert\nContent-Type: text/html")" "$clonemaker"
