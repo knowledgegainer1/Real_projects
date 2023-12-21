f77aa2c8e5aa5a62aa2cf1cb22f369c0d4942054
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-21 22:12:23 +0530

    added

--- Opt-agrs.sh
+++ Opt-agrs.sh
@@ -6,25 +6,24 @@ wishes=""
 usage() {
     echo "usage :: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
-    echo " -n, Specify the name (mandatory)"
-    echo " -w, Specify the wishes. ex, Good Morning"
-    echo " -h, Display Help and exit"
+    echo " -n ,Specify the name (mandatory)"
+    echo " -w ,Specify the wishes. ex,Good Morning"
+    echo " -h,Display Helpand exit"
 }
 
 while getopts ":n:w:h" opt; do
     case $opt in
-        n) name="$OPTARG";;
-        w) wishes="$OPTARG";;
-        h) usage; exit;;
-        :) echo "Option -$OPTARG requires an argument." >&2; usage; exit 1;;
-        \?) echo "Invalid option: -$OPTARG" >&2; usage; exit 1;;
+    n) name="$OPTARG";;
+    w) wishes="$OPTARG";;
+    h) usage; exit;;
+    :) usage; exit 1;;
+    \?) echo "invalid options: -"$OPTARG"" >&2; usage; exit 1;;
     esac
 done
 
 if [ -z "$name" ] || [ -z "$wishes" ]; then
-    echo "error: both -n and -w should be passed"
+    echo "error:both -n & -w should be passed"
     usage
-    exit 1
+    exit 1 
 fi
-
-echo "Hello, $name. $wishes. Welcome to the class"
+echo "Hello, $name . $wishes ,Welcome to the class"
