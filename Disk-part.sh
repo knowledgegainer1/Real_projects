b94e825675a9c80f7bcdbd94c5c56b7c533210e4
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-21 20:58:30 +0530

    added

--- Disk-part.sh
+++ Disk-part.sh
@@ -8,7 +8,7 @@ while IFS= read line; do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $disk_treshold ]; then
-        message+="high disk usage on $partition: $usage <br>"
+        message+="high disk usage on $partition: $usage <br4>"
     fi
 done <<<$disk_usage
 
---
+++ Opt-agrs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+name=""
+wishes=""
+
+usage() {
+    echo "usage :: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n ,Specify the name (mandatory)"
+    echo " -w ,Specify the wishes. ex,Good Morning"
+    echo " -h,Display Helpand exit"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+    n) name="$optarg" ;;
+    w) wishes="$optarg" ;;
+    h | *)
+        usage;
+        exit
+        ;;
+    esac
+done
