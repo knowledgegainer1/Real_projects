dba8674b2c1992f1e8f584dc902d671d11f658ef
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-22 15:40:54 +0530

    aded

--- roboshop.sh
+++ roboshop.sh
@@ -6,12 +6,12 @@ instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "shipping" "payment"
 
 for i in "${instances[@]}"
  do
-    if [ $i == "mongodb"] || [ $i == "mysql" ] ||
-        [ $i == "shipping" ]; then
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] ||
+        [ $i == "shipping" ]
+    then
         instance_type="t3.small"
     else
         instance_type="t2.micro"
     fi
     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_type --security-group-ids sg-0a719cdd2c1f8d094
 done
- 
